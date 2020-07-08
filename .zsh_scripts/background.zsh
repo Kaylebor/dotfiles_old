58d9703b3dc73bc7eaaecf72e0272f99aeff99fe
Ender Veiga Bueno <letstalk@ender.codes>
2020-07-08 23:29:29 +0200

    Improved background.zsh and added environment variables for configuration

--- .zsh_scripts/background.zsh
+++ .zsh_scripts/background.zsh
@@ -1,12 +1,49 @@
 #!/bin/zsh
 
-if [[   -f $(which docker) && \
-        ! -z $(docker image ls | rg kaylebor/eclipse.jdt.ls) && \
-        -z $(docker container ps -f 'status=running' -f 'ancestor=kaylebor/eclipse.jdt.ls' | rg --invert-match IMAGE | tr -s ' ' | cut -d ' ' -f 2) ]]; then
-    prev_container=$(docker container ps -a -f 'ancestor=kaylebor/eclipse.jdt.ls' | rg --invert-match IMAGE | tail -n1 | tr -s ' ' | cut -d ' ' -f 1)
-    if [[ -z $prev_container ]]; then
-        docker run -d --net=host kaylebor/eclipse.jdt.ls 2>&1 > /dev/null
-    else
-        docker start $prev_container 2>&1 > /dev/null
+check_not_running() {
+    unset not_running
+    if [[ -f $(which docker) && \
+        ! -z $(docker image ls | rg $1) && \
+        -z $(docker container ps -f 'status=running' -f 'ancestor=$1' | rg --invert-match IMAGE | tr -s ' ' | cut -d ' ' -f 2) ]]; then
+        not_running=1
+        prev_container=$(docker container ps -a -f "ancestor=$1" | rg --invert-match IMAGE | tail -n1 | tr -s ' ' | cut -d ' ' -f 1)
+    fi
+}
+
+if [[ ! -z ENABLE_JDT ]]; then
+    jdt_container="kaylebor/eclipse.jdt.ls"
+    check_not_running $jdt_container
+    if [[ ! -z not_running  ]]; then
+        if [[ -z $prev_container ]]; then
+            docker run -d --net=host $jdt_container 2>&1 > /dev/null
+        else
+            docker start $prev_container 2>&1 > /dev/null
+        fi
+    fi
+fi
+
+if [[ ! -z ENABLE_JELLYFIN ]]; then
+    jellyfin_container="jellyfin/jellyfin"
+    check_not_running $jellyfin_container
+    if [[ ! -z not_running  ]]; then
+        if [[ -z $prev_container ]]; then
+            [[ -z $(docker volume ls | rg jellyfin-config) ]] && docker volume create jellyfin-config
+            [[ -z $(docker volume ls | rg jellyfin-cache) ]] && docker volume create jellyfin-cache
+            docker run -d \
+                -p 8096:8096 \
+                -p 8920:8920 \
+                -e NVIDIA_DRIVER_CAPABILITIES=all \
+                -e NVIDIA_VISIBLE_DEVICES=all \
+                --runtime=nvidia \
+                --gpus all \
+                -v jellyfin-config:/config \
+                -v jellyfin-cache:/cache \
+                -v /home/media:/media \
+                --user 1000:1000 \
+                --restart unless-stopped \
+                jellyfin/jellyfin:latest 2>&1 > /dev/null
+        else
+            docker start $prev_container 2>&1 > /dev/null
+        fi
     fi
 fi
--- .zsh_scripts/funcs.zsh
+++ .zsh_scripts/funcs.zsh
@@ -3,12 +3,6 @@ passgen() {
   < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;
 }
 
-jellyfin_start() {
-  [[ -z $(docker volume ls | rg jellyfin-config) ]] && docker volume create jellyfin-config
-  [[ -z $(docker volume ls | rg jellyfin-cache) ]] && docker volume create jellyfin-cache
-  docker run -d -v jellyfin-config:/config -v jellyfin-cache:/cache -v /home/media:/media --user 1000:1000 --net=host jellyfin/jellyfin:latest
-}
-
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
