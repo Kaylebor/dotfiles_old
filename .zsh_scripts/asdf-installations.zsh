a63580d99c3bb2d453380d6cf40cb8a20ed3b10d
Ender Veiga Bueno <letstalk@ender.codes>
2020-07-08 22:31:20 +0200

    Removed java from automatic asdf installs

--- .zsh_scripts/asdf-installations.zsh
+++ .zsh_scripts/asdf-installations.zsh
@@ -22,7 +22,7 @@ asdf_update_latest() {
 }
 
 if [[ -f $(which asdf) ]]; then
-    for plugin in rust golang java
+    for plugin in rust golang
     do
         asdf_install_latest $plugin
     done
