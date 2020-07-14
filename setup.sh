c1527c24d0cae629314d83fa61b560e667684325
Ender Veiga Bueno <letstalk@ender.codes>
2020-07-14 14:28:44 +0200

    Improved setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/zsh
+PREV_DIR=$(pwd)
+cd $(cd -P -- "$(dirname -- "$0")" && pwd -P)
+DIR="$(pwd)"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+[[ ! -d "$HOME/.backups" ]] && mkdir -p "$HOME/.backups"
 
-[[ -f "$HOME/.zshrc" ]] && mv "$HOME/.zshrc" "$HOME/.zshrc-backup"
-ln -s "$DIR/.zshrc" "$HOME/.zshrc"
-[[ -f "$HOME/.p10k.zsh" ]] && mv "$HOME/.p10k.zsh" "$HOME/.p10k.zsh-backup"
-ln -s "$DIR/.p10k.zsh" "$HOME/.p10k.zsh"
-[[ -f "$HOME/.zsh_scripts" ]] && mv "$HOME/.zsh_scripts" "$HOME/.zsh_scripts-backup"
-ln -s "$DIR/.zsh_scripts" "$HOME/.zsh_scripts"
-[[ -f "$HOME/.gitconfigs" ]] && mv "$HOME/.gitconfigs" "$HOME/.gitconfigs-backup"
-ln -s "$DIR/.gitconfigs" "$HOME/.gitconfigs"
+for file in .zshrc .p10k.zsh .zsh_scripts .gitconfigs .tool-versions; do
+    [[ -e "$HOME/$file" ]] && mv "$HOME/$file" "$HOME/.backups/"
+    ln -s "$DIR/$file" "$HOME/"
+done
+
+cd $HOME
+asdf install
+cd $PREV_DIR
+# source $HOME/.zshrc
