1d3ca209c317ea1f9709bfc06b81fd74a446dcb6
Ender Veiga Bueno <letstalk@ender.codes>
2020-07-15 10:20:06 +0200

    Changed background Docker images to use docker-compose.yml instead

--- .zsh_scripts/background.zsh
+++ .zsh_scripts/background.zsh
@@ -8,51 +8,19 @@ check-previous-container-name() {
     echo $(docker container ps -a -f "ancestor=$1" | rg --invert-match IMAGE | tail -n1 | tr -s ' ' | cut -d ' ' -f 1)
 }
 
+PREV_DIR=$(pwd)
+cd $(cd -P -- "$(dirname -- "$0")" && pwd -P)/docker
+
 if [[ $ENABLE_JDT -eq 1 ]]; then
-    container_name="kaylebor/eclipse.jdt.ls"
-    if [[ $(check-running-containers $container_name) -lt 1  ]]; then
-        prev_container=$(check-previous-container-name $container_name)
-        if [[ -z $prev_container ]]; then
-            docker run -d --net=host $container_name 2>&1 > /dev/null
-        else
-            docker start $prev_container 2>&1 > /dev/null
-        fi
-    fi
+    docker-compose up -d jdt
 fi
 
 if [[ $ENABLE_JELLYFIN -eq 1 ]]; then
-    container_name="jellyfin/jellyfin"
-    if [[ $(check-running-containers $container_name) -lt 1  ]]; then
-        prev_container=$(check-previous-container-name $container_name)
-        if [[ -z $prev_container ]]; then
-            [[ -z $(docker volume ls | rg jellyfin-config) ]] && docker volume create jellyfin-config
-            [[ -z $(docker volume ls | rg jellyfin-cache) ]] && docker volume create jellyfin-cache
-            docker run -d \
-                --net=host \
-                -e NVIDIA_DRIVER_CAPABILITIES=all \
-                -e NVIDIA_VISIBLE_DEVICES=all \
-                --runtime=nvidia \
-                --gpus all \
-                -v jellyfin-config:/config \
-                -v jellyfin-cache:/cache \
-                -v /home/media:/media \
-                --user 1000:1000 \
-                --restart unless-stopped \
-                $container_name:nightly 2>&1 > /dev/null
-        else
-            docker start $prev_container 2>&1 > /dev/null
-        fi
-    fi
+    docker-compose up -d jellyfin
 fi
 
 if [[ $ENABLE_JUPYTER_MINIMAL -eq 1 ]]; then
-    container_name="jupyter/minimal-notebook"
-    if [[ $(check-running-containers $container_name) -lt 1  ]]; then
-        prev_container=$(check-previous-container-name $container_name)
-        if [[ -z $prev_container ]]; then
-            docker run -d -e JUPYTER_ENABLE_LAB=yes -p 8888:8888 $container_name
-        else
-            docker start $prev_container 2>&1 > /dev/null
-        fi
-    fi
+    docker-compose up -d jupyter-minimal
 fi
+
+cd $PREV_DIR
