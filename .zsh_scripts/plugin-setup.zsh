a662b3c37fc6849be8e7a515003daa90079e0f7b
Ender Veiga Bueno <letstalk@ender.codes>
2020-07-28 22:57:20 +0200

    Added Neovim configuration file

--- .zsh_scripts/plugin-setup.zsh
+++ .zsh_scripts/plugin-setup.zsh
@@ -3,11 +3,11 @@
 
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
-        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+  print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+  command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+  command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+    print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+    print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
 
 ### End of Zinit's installer chunk
@@ -20,10 +20,10 @@ autoload -Uz _zinit
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit light-mode for \
-    zinit-zsh/z-a-rust \
-    zinit-zsh/z-a-as-monitor \
-    zinit-zsh/z-a-patch-dl \
-    zinit-zsh/z-a-bin-gem-node
+  zinit-zsh/z-a-rust \
+  zinit-zsh/z-a-as-monitor \
+  zinit-zsh/z-a-patch-dl \
+  zinit-zsh/z-a-bin-gem-node
 
 # powerlevel10k theme
 zinit light-mode depth=1 for romkatv/powerlevel10k
@@ -33,8 +33,8 @@ zinit light-mode depth=1 for romkatv/powerlevel10k
 zinit load mroth/evalcache
 
 zinit wait lucid light-mode for \
-    atinit"zicompinit; zicdreplay" zdharma/fast-syntax-highlighting \
-    atload"_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
+  atinit"zicompinit; zicdreplay" zdharma/fast-syntax-highlighting \
+  atload"_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
 
 # Plugin history-search-multi-word loaded with investigating.
 zinit wait lucid for zdharma/history-search-multi-word
@@ -49,15 +49,15 @@ zinit pack for ls_colors
 # extract -> abstracts specifics about extracting compressed files behind a single 'extract' command
 # cp      -> adds a function cpv that uses rsync to copy
 zinit wait lucid for \
-    OMZ::plugins/extract \
-    OMZ::plugins/cp
+  OMZ::plugins/extract \
+  OMZ::plugins/cp
 
 # Installs vim-plug
 vimplugpath="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload"
 zinit wait lucid as"null" atpull"!mkdir -p $vimplugpath" cp"plug.vim -> $vimplugpath/plug.vim" for junegunn/vim-plug
 
 zinit wait lucid as"program" atclone'perl Makefile.PL PREFIX=$ZPFX' \
-        atpull'%atclone' make'install' pick"$ZPFX/bin/git-cal" for light-mode k4rthik/git-cal
+  atpull'%atclone' make'install' pick"$ZPFX/bin/git-cal" for light-mode k4rthik/git-cal
 
 # Gitignore plugin – commands gii and gi
 zinit wait'2' lucid for voronkovich/gitignore.plugin.zsh
@@ -69,9 +69,13 @@ zinit pick'asdf.sh' for light-mode @asdf-vm/asdf
 zinit from'gh-r' as'program' bpick'*linux-amd64*' pick'*/bin/hub' atclone'rm hub-linux-*/share/man/man1/*.txt' for @github/hub
 
 zinit wait lucid from'gh-r' as'program' for \
-    bpick'*x86_64*' pick'*/bin/googler' jarun/googler
+  bpick'*x86_64*' pick'*/bin/googler' jarun/googler
 
 zinit wait lucid atload"zicompinit; zicdreplay" blockf for \
-    zsh-users/zsh-completions
+  zsh-users/zsh-completions
+
+[[ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload" ]] && mkdir -p ${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload
+zinit wait lucid as'null' atclone"cp plug.vim ${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" for \
+  junegunn/vim-plug
 
 zinit wait lucid light-mode for blockf atpull'zinit creinstall -q .' zsh-users/zsh-completions
--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ DIR="$(pwd)"
 [[ ! -d "$HOME/.backups" ]] && mkdir -p "$HOME/.backups"
 
 for file in .zshrc .p10k.zsh .zsh_scripts .gitconfigs .tool-versions; do
-    [[ -e "$HOME/$file" ]] && mv "$HOME/$file" "$HOME/.backups/"
-    ln -s "$DIR/$file" "$HOME/"
+  [[ -e "$HOME/$file" ]] && mv "$HOME/$file" "$HOME/.backups/"
+  ln -s "$DIR/$file" "$HOME/"
 done
 
 cd $HOME
@@ -15,7 +15,7 @@ asdf install
 
 # Installing Rust packages
 for package in exa hexyl; do
-    [[ ! -f $(which $package) ]] && cargo install package # sharkdp/fd
+  [[ ! -f $(which $package) ]] && cargo install package # sharkdp/fd
 done
 [[ ! -f $(which bat) ]] && cargo install --locked bat # sharkdp/bat
 [[ ! -f $(which fd) ]] && cargo install fd-find # sharkdp/fd
@@ -25,9 +25,14 @@ asdf reshim rust
 # Installing Node packages
 node-check-installed() { yarn global list --depth=0 | rg $1 }
 for package in tldr; do
-    [[ -z $(node-check-installed $package) ]] && yarn global add $package
+  [[ -z $(node-check-installed $package) ]] && yarn global add $package
 done
 asdf reshim nodejs
+
+if [[ ! -f $HOME/.config/nvim/init.vim ]]; then
+  mkdir -p $HOME/.config/nvim/
+  ln -s $DIR/init.vim $HOME/.config/nvim/init.vim
+fi
 
 cd $PREV_DIR
 source $HOME/.zshrc
