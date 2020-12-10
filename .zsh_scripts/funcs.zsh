f60556e1f1ac6b371e44164e347112ad47a38cc4
Ender Veiga Bueno <letstalk@ender.codes>
2020-12-10 12:28:51 +0100

    Added function to search if a specified html id exists in the current directory

--- .zsh_scripts/funcs.zsh
+++ .zsh_scripts/funcs.zsh
@@ -3,6 +3,10 @@ passgen() {
   < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;
 }
 
-search-class() {
+html-search-class() {
   rg -thtml "class=\"[a-zA-Z0-9-]* ?$1 ?[a-zA-Z0-9-]*\""
+}
+
+html-search-id() {
+  rg -thtml "id=\"$1\""
 }
