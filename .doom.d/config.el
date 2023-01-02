3ac9606c56e63bd06cd5cd05d21a98b0533367e9
Ender Veiga Bueno <letstalk@ender.codes>
2023-01-02 11:42:39 +0100

    Update Brewfile and dependencies
    Minor configuration changes on Doom Emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,22 +21,35 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 16)
-      doom-variable-pitch-font (font-spec :family "Source Sans Pro" :size 16))
+(setq doom-font (font-spec :family "Hack Nerd Font" :size 16)
+      doom-variable-pitch-font (font-spec :family "Hack Nerd Font" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq doom-theme 'doom-dark+)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'visual)
 
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
@@ -60,5 +73,19 @@
 ;; (after! projectile (setq projectile-project-root-files-bottom-up (remove
 ;;             ".git" projectile-project-root-files-bottom-up)))
 
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+            '(
+              (width . 130)
+              (height . 40)
+              (left . 80)
+              (top . 50)))
+      (setq default-frame-alist
+            '(
+              (width . 130)
+              (height . 40)
+              (left . 80)
+              (top . 50)))))
+
 (setq-default tab-width 2)
-(setq initial-frame-alist '((top . 1) (left . 1) (width . 120) (height . 40)))
